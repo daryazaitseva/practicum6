0a504722dba68bd81e4eced35207a03142644aca
daryazaitseva <zaytsevaart@gmail.com>
2024-03-27 22:21:06 +0700

    first

--- 1.py
+++ 1.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+# Zaitseva Darya 23704.1
+# Practicum 6
+#
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+a, b = map(int, input('Введите значения AxB: ').split('x'))
+if a < 13 and b < 13:
+    print('да')
+else:
+    print('нет')
