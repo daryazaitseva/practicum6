8a6e7ad2e693d5303eae954516de56e27e5713b8
daryazaitseva <zaytsevaart@gmail.com>
2024-03-30 13:27:54 +0700

    changes

--- 7.py
+++ 7.py
@@ -1,23 +1,19 @@
 k = int(input('Введите количество суш: '))
 ost = 0
-ostat = 0
-if k != 0 and k % 5 != 0 and k % 7 != 0:
+if k % 5 == 0 or k % 7 == 0:
+    print('да')
+else:
     while k > 0:
-        ost = k - 5
-        if ost == 7 or ost == 5:
+        k -= 5
+        if k % 5 == 0:
             print('да')
             break
-        k -= 5
-    if ost != 5 and ost != 7:
-        ost = 0
-        while k > 0:
-            ost = k - 7
-            if ost == 7 or ost == 5:
-                print('да')
-                break
-            k -= 7
-        if ost != 0 or ost != 5 and ost != 7:
-            print('нет')
-elif k % 5 == 0 or k % 7 == 0:
-    print('да')
+        elif k % 7 == 0:
+            print('да')
+            break
+    if k % 5 != 0 and k % 7 != 0:
+        print('нет')
+
+
+
 
